35f94ca34d9deee81d25e08e9b3e8fa8cbd77299
Postimal <michal.postek1@wp.pl>
2020-01-08 11:11:10 +0100

    Updates

---
+++ precache-manifest.a1da82700a4fa1e8b368ee4b951b924e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0281959f1124154e02728a7fb0ba5d2",
+    "url": "/Twitch-API/index.html"
+  },
+  {
+    "revision": "bd426d5fad0a645f53b2",
+    "url": "/Twitch-API/static/css/main.99497a40.chunk.css"
+  },
+  {
+    "revision": "6fb71f5173cc30bfc628",
+    "url": "/Twitch-API/static/js/2.4ecb1fe6.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Twitch-API/static/js/2.4ecb1fe6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bd426d5fad0a645f53b2",
+    "url": "/Twitch-API/static/js/main.8e65c1e5.chunk.js"
+  },
+  {
+    "revision": "3435fc63283544bb916b",
+    "url": "/Twitch-API/static/js/runtime-main.38000a02.js"
+  },
+  {
+    "revision": "872a7ac147d1ca33361af6d1e3cfa262",
+    "url": "/Twitch-API/static/media/error404.872a7ac1.jpg"
+  }
+]);
